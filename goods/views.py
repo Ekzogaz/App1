d3b568b4d3e90504f8e783549bddc52b1a3ab1b5
Ekzogaz <Ekzogaz2013@gmail.com>
2024-12-24 10:28:18 +0200

    Slug converter to

--- goods/views.py
+++ goods/views.py
@@ -15,8 +15,10 @@ def catalog(request):
 
     return render(request, 'goods/catalog.html', context)
  
-def product(request): 
+def product(request , product_slug): 
+    product = Products.objects.get(slug=product_slug)
     context = {
-        "title": 'Home - Товар',
+        
+        "product": product,
     }
-    return render(request, 'goods/product.html', context)
+    return render(request, 'goods/product.html', context=context)
