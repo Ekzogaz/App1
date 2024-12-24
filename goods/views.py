db097610d741fb7f5b08695c8e32dcdab849ca5d
Ekzogaz <Ekzogaz2013@gmail.com>
2024-12-24 14:18:15 +0200

    the first pagination options

--- goods/views.py
+++ goods/views.py
@@ -1,21 +1,26 @@
 from decimal import Context
-from urllib import request
-from django.shortcuts import get_object_or_404, render
+from django.core import paginator
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.template import context
 from goods.models import Products
 
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     if category_slug == 'all':
         goods=Products.objects.all()
     else:
-        goods = get_object_or_404(Products, category__slug=category_slug)
-    
+        goods = get_list_or_404(Products, category__slug=category_slug)
+
+    paginator=Paginator(goods, 3) 
+    curentpage=paginator.page(page)
+      
 
 
     context = {
         "title": 'Home - Каталог',
-        "goods": goods,
+        "goods": curentpage,
+        "slug_url": category_slug,
     }
 
     return render(request, 'goods/catalog.html', context)
