cba4ecfb01ee894fb9a5f859511a90a110620052
Ekzogaz <Ekzogaz2013@gmail.com>
2024-12-29 12:47:49 +0200

    Filter ok

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,6 +1,8 @@
 from atexit import register
 from django import template
 from goods.models import Categories
+from django.utils.http import urlencode
+from django.http import QueryDict
 
 register = template.Library()
 
@@ -9,5 +11,13 @@ register = template.Library()
 
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+
+def change_params(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.update(kwargs)
+    return query.urlencode()
+
 
 
